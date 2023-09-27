e14ed89d7c00c545a13102d5ae2ef555dcfe7a2d
Ayoub <ayoubchebbi570@gmail.com>
2023-09-27 21:57:23 +0100

    changes to mern project

--- client/src/Components/Admin-Users/AdminUsers.jsx
+++ client/src/Components/Admin-Users/AdminUsers.jsx
@@ -1,61 +1,69 @@
-import React, { useEffect, useState } from 'react'
-import "./AdminUsers.css"
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-import SideBar from '../AdminSideBar/SideBar'
+import React, { useEffect, useState } from 'react';
+import './AdminUsers.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import SideBar from '../AdminSideBar/SideBar';
+
 const AdminUsers = () => {
-  const [users , setUsers] = useState([])
+  const [users, setUsers] = useState([]);
+
   const handleDelete = (userId) => {
-    axios.delete(`http://localhost:8000/api/user/${userId}`)
-        .then(() => {
-            const filtredUsers = users.filter((eachUser) => {
-                return eachUser._id !== userId;
-            });
-            setUsers(filtredUsers);
-        })
-        .catch((error) => {
-            console.error("Error deleting User:", error);
+    axios
+      .delete(`http://localhost:8000/api/user/${userId}`)
+      .then(() => {
+        const filteredUsers = users.filter((eachUser) => {
+          return eachUser._id !== userId;
         });
-};
-  useEffect(()=>{
-    axios.get("http://localhost:8000/api/allusers")
-      .then(res=>{
-        setUsers(res.data)
+        setUsers(filteredUsers);
       })
-  },[])
+      .catch((error) => {
+        console.error("Error deleting User:", error);
+      });
+  };
+
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/allusers")
+      .then(res => {
+        setUsers(res.data);
+      });
+  }, []);
+
   return (
     <div className='flex-admin_users'>
       <SideBar />
-      <div className='users-info'>
-        <h1>Users</h1>
-      {users.map((user)=>{
-        return <div>
-          <table>
+      <div className="table-container">
+        <table className="data-table">
+          <thead>
             <tr>
-              <td>first Name</td>
-              <td>last Name</td>
-              <td>Email</td>
-              <td>Account type</td>
-              <td>Action</td>
+              <th>First Name</th>
+              <th>Last Name Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
             </tr>
-            <tr>
-              <td>{user.firstName}</td>
-              <td>{user.lastName}</td>
-              <td>{user.email}</td>
-              <td>{user.accountType}</td>
-              <td>
-                <button onClick={() => { handleDelete(user._id) }} style={{marginRight : "5px"}}>
-                  delete
-                </button>
-                <Link to={`/admin/user/edit/${user._id}`}>edit</Link>
-              </td>
-            </tr>
-          </table>
-        </div>
-      })}
+          </thead>
+          <tbody>
+            {users.map((item, index) => (
+              <tr key={index}>
+                <td>{item.firstName}</td>
+                <td>{item.lastName}</td>
+                <td>{item.email}</td>
+                <td>{item.accountType}</td>
+                <td>
+                  <button onClick={() => { handleDelete(item._id) }}>
+                    <i className="fa fa-trash-o" style={{ fontSize: "36px" }}></i>
+                  </button>
+                  <Link to={`/admin/user/edit/${item._id}`}>
+                    edit
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
-  )
+  );
 }
 
-export default AdminUsers
+export default AdminUsers;
